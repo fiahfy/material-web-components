ecf198b5163014e11b5582e3828bd0fa8ae96b6b
fiahfy <d.fiahfy@gmail.com>
2020-09-16 23:44:16 +0900

    Update components

--- src/button/index.ts
+++ src/button/index.ts
@@ -3,27 +3,49 @@ import { MDCRipple } from '@material/ripple'
 // @ts-ignore
 import css from 'bundle-text:./index.scss'
 
+const template = document.createElement('template')
+template.innerHTML = `
+  <style>${css}</style>
+  <script src="https://unpkg.com/material-components-web@latest/dist/material-components-web.min.js"></script>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <button class="mdc-button">
+    <div class="mdc-button__ripple"></div>
+    <slot name="leading-icon"></slot>
+    <span class="mdc-button__label"><slot /></span>
+  </button>
+`
+
 class Button extends HTMLElement {
   static get observedAttributes() {
     return ['disabled', 'variant']
   }
   constructor() {
     super()
-    const shadow = this.attachShadow({ mode: 'open' })
-    shadow.innerHTML = `
-      <style>${css}</style>
-      <script src="https://unpkg.com/material-components-web@latest/dist/material-components-web.min.js"></script>
-      <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-      <button class="mdc-button">
-        <div class="mdc-button__ripple"></div>
-        <span class="mdc-button__label"><slot /></span>
-      </button>
-    `
+    this.attachShadow({ mode: 'open' })
+    this.shadowRoot?.append(template.content.cloneNode(true))
+    this.shadowRoot
+      ?.querySelector('slot')
+      ?.addEventListener('slotchange', (e) => {
+        console.log(e)
+        if (!(e.currentTarget instanceof HTMLSlotElement)) {
+          return
+        }
+        e.currentTarget.assignedNodes().forEach((node) => {
+          if (!(node instanceof HTMLElement)) {
+            return
+          }
+          console.log(node)
+          node.classList.add('mdc-button__icon')
+          node.setAttribute('aria-hidden', 'true')
+        })
+      })
   }
   connectedCallback() {
-    const shadow = this.shadowRoot
-    const button = shadow?.querySelector('button')
-    button && new MDCRipple(button)
+    const button = this.shadowRoot?.querySelector('button')
+    if (!button) {
+      return
+    }
+    new MDCRipple(button)
   }
   disconnectedCallback() {
     //
@@ -32,8 +54,10 @@ class Button extends HTMLElement {
     //
   }
   attributeChangedCallback() {
-    const shadow = this.shadowRoot
-    const button = shadow!.querySelector('button')! // eslint-disable-line @typescript-eslint/no-non-null-assertion
+    const button = this.shadowRoot?.querySelector('button')
+    if (!button) {
+      return
+    }
     // disabled
     button.disabled = this.getAttribute('disabled') !== null
     // variant
@@ -62,4 +86,5 @@ class Button extends HTMLElement {
     }
   }
 }
+
 customElements.define('m-button', Button)
--- src/icon/index.ts
+++ src/icon/index.ts
@@ -1,13 +1,18 @@
+const template = document.createElement('template')
+template.innerHTML = `
+  <script src="https://unpkg.com/material-components-web@latest/dist/material-components-web.min.js"></script>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <i class="material-icons"><slot /></i>
+`
+
 class Icon extends HTMLElement {
   static get observedAttributes() {
     return []
   }
   constructor() {
     super()
-    const shadow = this.attachShadow({ mode: 'open' })
-    shadow.innerHTML = `
-      <i class="material-icons">add</i>
-    `
+    this.attachShadow({ mode: 'open' })
+    this.shadowRoot?.append(template.content.cloneNode(true))
   }
   connectedCallback() {
     //
@@ -22,4 +27,5 @@ class Icon extends HTMLElement {
     //
   }
 }
+
 customElements.define('m-icon', Icon)
